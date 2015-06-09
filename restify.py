08b33bf39289da69fe13d4f68db946ec441b8752
Krish <krishna.sunuwar@gmail.com>
2015-06-09 15:34:44 +0545

    removal of endpoints and added restify.py

--- restify.py
+++ restify.py
@@ -1,20 +1,27 @@
 import re
 import unicodedata
 
+from rest_framework.routers import DefaultRouter
+
 from importlib import import_module
 from django.utils.module_loading import module_has_submodule
 from django.conf import settings
 from django.db.models.base import ModelBase
 
 from .viewsets import get_viewsets
+from .users import UserViewSet
 
 class Restify(object):
 
     def __init__(self):
-        self.IGNORE_LIST = ('^django*', '^api$')  # default ignore list
+        self.IGNORE_LIST = ('^django*', '^api$', '^rest_framework*',
+                            '^auth*')  # default ignore list
+        self.router = None
         self.viewsets = {}   # viewsets
         self.module = ()   # keep track of imported Django App (installed App)
         self.model = ()   # keep track of imported model of an Django App
+
+        self.apps()
 
     def slugify(self, value):
         try:
@@ -34,48 +41,58 @@ class Restify(object):
                 continue
 
             app = import_module(entry)
-            self.module += (entry, )   # for preventing reimport
+            self.module += (entry, )
+
+            # print entry
 
             if module_has_submodule(app, 'models'):
                 models_module_name = '%s.%s' % (entry, 'models')
 
-                # do not import model if it already imported
-                if models_module_name in self.model:
-                    continue
-
                 models = import_module(models_module_name)
 
-                self.model += (models_module_name, )  # for preventing reimport
-
                 for mod in dir(models):
+
+                    # do not import model if it already imported
+                    if mod in self.model:
+                        continue
+
+                    # skip auth User model
+                    if mod.lower() == 'user':
+                        continue
+
+                    self.model += (mod, )
+
                     try:
                         mod2 = getattr(models, mod)
                     except:
                         continue
 
-                    # print "mod ", mod2._meta.abstract
-
                     if not isinstance(mod2, ModelBase) or getattr(mod2._meta, 'abstract', None):
                         continue
 
-                    # print mod
-
-                    # actual model to register api
-                    # print mod2._meta.verbose_name_plural.title()
                     url = self.slugify(mod2._meta.verbose_name_plural.title())
                     # print url
                     viewset = get_viewsets(mod2)
 
-                    if url == 'users':
-                        viewset.serializer_class.Meta.exclude = ('password', )
-
                     self.viewsets[url] = viewset
 
+        print self.model
+
     def register(self):
-        if url == 'users':
-            viewset.serializer_class.Meta.exclude = ('password', )
-        else:
-            router.register(url, viewset)
+
         # print self.viewsets
 
+        self.router = DefaultRouter()
+        for url, viewset in self.viewsets.iteritems():
+            self.router.register(url, viewset)
+
+        # special case for auth User model
+        self.router.register('users', UserViewSet)
+
+        return self.router
+
+    def router(self):
+        return self.router
+
 restify = Restify()
+router = restify.register()
